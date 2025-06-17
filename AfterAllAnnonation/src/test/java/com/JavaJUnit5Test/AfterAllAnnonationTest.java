e13dc6ff57322f77d3d36ad69b3c4a22cd9fa581
ladharsh726 <ladharsh726@gmail.com>
2025-06-17 18:12:12 -0400

    Completed "AferAll Annotation"

--- AfterAllAnnonation/src/test/java/com/JavaJUnit5Test/AfterAllAnnonationTest.java
+++ AfterAllAnnonation/src/test/java/com/JavaJUnit5Test/AfterAllAnnonationTest.java
@@ -2,11 +2,12 @@ package com.JavaJUnit5Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BeforeAllAnnotationTest {
+class AfterAllAnnonationTest {
 	
 	ComputeAreaOfShapes cs;
 	
@@ -15,21 +16,26 @@ class BeforeAllAnnotationTest {
 		System.out.println("<===== BeforeAll called =====> \n");
 	}
 
-	@BeforeEach
+	@AfterEach
 	void setUp() {
 		 cs = new ComputeAreaOfShapes();
-		 System.out.println("BeforeEach called");
+		 System.out.println("AfterEach called");
 	}
 
 	@Test
 	void testComputeSquareArea() {
-		assertNotEquals(576, cs.squareArea(5), ()-> "Area of Square test case has failed.");
+		//assertEquals(576, cs.squareArea(5), ()-> "Area of Square test case has failed.");
 		System.out.println("Test method called");
 	}
 	
 	@Test
 	void testComputeCircleArea() {
-		assertEquals(78.5, cs.circleArea(5), ()-> "Area of Circle test case has failed.");
+		//assertEquals(78.5, cs.circleArea(5), ()-> "Area of Circle test case has failed.");
 		System.out.println("Test method called");
+	}
+	
+	@AfterAll
+	static void tearDownAfterClass() {
+		System.out.println("\n<===== AfterAll called =====>");
 	}
 }
